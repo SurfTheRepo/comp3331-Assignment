19ca52058297d0246619c3ca01bfc4628552b1d5
Otto <z5116011@student.unsw.edu.au>
2018-09-28 14:01:42 +1000

    add basic udp function to server and reciever

--- receiver.py
+++ receiver.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+import socket
+import sys
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+server_address = ('localhost', 8888)
+print('startig up on {} port {}'.format(*server_address))
+
+sock.bind(server_address)
+
+
+
+while True:
+	print('\nwaiting to recieve message')
+	data, address = sock.recvfrom(4096)
+
+	print('received {} bytes from {}'.format(len(data), address))
+	print(data)
--- sender.py
+++ sender.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3\
+#!/usr/bin/python3
+
+import socket
+import sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = ('localhost', 8888)
+message = b'this is the message. it will be repeated.'
+
+try:
+	print('sending {!r}'.format(message))
+	sent = sock.sendto(message, server_address)
+finally:
+	print('closing socket')
+	sock.close()
